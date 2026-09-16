0ae6233ec569cb869f821187cf4d49bd0d815ba0
ladislavochka <ladislavochka@gmail.com>
2026-09-16 18:55:12 +0300

    Vlad

--- Vlad/F1line.py
+++ Vlad/F1line.py
@@ -1,51 +1,75 @@
 import customtkinter as ctk
+from pygame import mixer
+from PIL import Image
+
+mixer.init()
 
 app = ctk.CTk()
 app.title("Filine")
 app.geometry("1300x800")
 
-# Заголовок
-title_name = ctk.CTkLabel(app, text="Filine", font=("arial", 30))
-title_name.pack(anchor="e", padx=20, pady=20)
+def three():
+    try:
+        mixer.music.load("WED_19-Reliz/Vlad/Sounds/Cult_Member_-_three_(SkySound7.com).mp3")
+        mixer.music.play()
+    except:
+        pass
 
-#!!ВСЯ КООРДИНАТА Х ОТ НАЧАЛА ДО КОНЦА ОКНА ПО ОСИ У ОТ СЛАЙДЕРА ДО КНОПОК
-knopki_frame = ctk.CTkFrame(app, fg_color="transparent")
+def RAC():
+    try:
+        mixer.music.load("WED_19-Reliz/Vlad/Sounds/SEMATARY - RAGING WOLF.mp3")
+        mixer.music.play()
+    except:
+        pass
 
-knopki_frame.pack(side="bottom", fill="x", pady=40)
+def magic():
+    try:
+        mixer.music.load("WED_19-Reliz/Vlad/Sounds/Medasin-magic-spaces.im.mp3")
+        mixer.music.play()
+    except:
+        pass
 
-#!!ПЛАТФОРМА В КОТОРОЙ КНОПКИ БЕЗ СЛАЙДЕРА!!
-buttons_frame = ctk.CTkFrame(knopki_frame, fg_color="transparent")
-buttons_frame.pack(pady=10)
+def pause_music():
+    mixer.music.pause()
 
+# ---ПЛЕЙЛІСТ (ФРЕЙМ)---
+playlist = ctk.CTkFrame(app, width=300, corner_radius=0)
+playlist.pack(side="left", fill="y")
 
-remote_button = ctk.CTkButton(buttons_frame, text="Минулий трек", width=120)
-remote_button.pack(side="left", padx=10)
+title_name = ctk.CTkLabel(playlist, text="Filine", font=("Arial", 30, "bold"))
+title_name.pack(padx=20, pady=20, anchor="w")
+photo_frame = ctk.CTkFrame(app, width=300, corner_radius=0)
+photo_frame.pack(side="right", fill="y")
 
-play_button = ctk.CTkButton(buttons_frame, text="▶Почати", width=80)
-play_button.pack(side="left", padx=10)
+#------PIP IMAGE------
 
-pause_button = ctk.CTkButton(buttons_frame, text="⏸Пауза", width=80)
+filine_avatar = Image.open("WED_19-Reliz/Vlad/filine.jpg")
+
+my_image = ctk.CTkImage(light_image=filine_avatar,dark_image=filine_avatar,size=(300, 300))
+
+photo_label = ctk.CTkLabel(photo_frame, image=my_image, text="")
+photo_label.pack(padx=20, pady=10, anchor="w")
+
+#------ПЛЕЙЛІСТ(КНОПКИ)------
+
+cult = ctk.CTkButton(playlist, text="Three-Cult_Member", width=200, command=three)
+cult.pack(padx=20, pady=10, anchor="w")
+
+wolf = ctk.CTkButton(playlist, text="SEMATARY - RAGING WOLF", width=200, command=RAC)
+wolf.pack(padx=20, pady=10, anchor="w")
+
+magik = ctk.CTkButton(playlist, text="Medasin-magic", width=200, command=magic)
+magik.pack(padx=20, pady=10, anchor="w")
+
+#--------КНОПКА ПАУЗА--------
+
+button_area = ctk.CTkFrame(app, fg_color="transparent")
+button_area.pack(side="right", fill="both", padx=20, pady=20)
+
+knopka_frame = ctk.CTkFrame(button_area, fg_color="transparent")
+knopka_frame.pack(side="bottom", fill="x", pady=20)
+
+pause_button = ctk.CTkButton(knopka_frame, text="⏸ Пауза", width=150, command=pause_music)
 pause_button.pack(side="left", padx=10)
-
-rewind_button = ctk.CTkButton(buttons_frame, text="Наступний трек", width=120)
-rewind_button.pack(side="left", padx=10)
-
-playlist = ctk.CTkFrame(app, fg_color="transparent")
-playlist.pack()
-playlist.place(relwidth=0.5, relheight=1)
-cult = ctk.CTkButton(playlist, text="Three-Cult_Member", width=175)
-cult.pack(anchor="w",pady=45)
-wolf = ctk.CTkButton(playlist, text="SEMATARY - RAGING WOLF", width=150)
-wolf.pack(anchor="w",pady=45)
-magik = ctk.CTkButton(playlist, text="Medasin-magic", width=175)
-magik.pack(anchor="w",pady=45)
-
-
-
-
-
-
-
-
 
 app.mainloop()
